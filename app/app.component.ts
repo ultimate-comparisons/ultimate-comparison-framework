8b649315c7de82f6c13f23b12ddee0743cf0a327
kleinech <kleinech.github@gmail.com>
2016-08-15 10:06:58 +0200

    Footer style

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { COMPARISON_DIRECTIVES, COMPARISON_PROVIDERS } from './components/comparison/index';
 
 @Component({
-    selector: 'my-app',
+    selector: 'myapp',
     template: '<comparison></comparison>',
     directives: [COMPARISON_DIRECTIVES],
     providers: [COMPARISON_PROVIDERS]
