700a5952222e2e768d5ed14d467a5271c5951968
kleinech <kleinech.github@gmail.com>
2016-09-17 17:53:43 +0200

    default label classes

--- app/components/comparison/shared/components/type.ts
+++ app/components/comparison/shared/components/type.ts
@@ -6,4 +6,16 @@ export class Type {
         public cls: string = "",
         public labelCls: LabelCls = new LabelCls()     
     ){}
+    
+    public getCls(item:string): string{
+        let labelClsString = this.labelCls.getCls(item);
+        if (this.cls == "" && labelClsString == ""){
+            return "label label-default";
+        } else if (labelClsString != ""){
+            return "label " + labelClsString;
+        } else {
+            //quick fix for old implementations
+            return this.cls != "label"? this.cls : this.cls + "label-default";
+        }   
+    }
 }
