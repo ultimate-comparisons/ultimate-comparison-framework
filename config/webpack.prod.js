69c73e3f9b814a832cd2318631b6d6d2f7e6294d
kleinech <kleinech.github@gmail.com>
2016-11-07 17:15:14 +0100

    Fix public path for webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path: helpers.root('www'),
-        publicPath: '/',
+        publicPath: '.',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[hash].chunk.js'
     },
