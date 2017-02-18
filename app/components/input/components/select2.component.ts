c5ba4fb92b0b10c36438dca1231cd08080aa2163
kleinech <kleinech.github@gmail.com>
2017-02-18 11:35:52 +0100

    Update of ng-select solves #9

--- app/components/input/components/select2.component.ts
+++ app/components/input/components/select2.component.ts
@@ -1,5 +1,4 @@
-import {Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'select2',
