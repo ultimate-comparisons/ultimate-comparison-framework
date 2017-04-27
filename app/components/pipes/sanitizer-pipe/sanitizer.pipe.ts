8b4e8580815965e194612987173e87a11326a770
Armin Hüneburg <hueneburg.armin@gmail.com>
2017-04-27 17:42:38 +0200

    Remove debug output

--- app/components/pipes/sanitizer-pipe/sanitizer.pipe.ts
+++ app/components/pipes/sanitizer-pipe/sanitizer.pipe.ts
@@ -11,7 +11,6 @@ export class SanitizerPipe implements PipeTransform {
     }
 
     transform(v: string): SafeHtml {
-        console.log("sanitize: " + v);
         let html = this._sanitizer.bypassSecurityTrustHtml(v);
         if (html.hasOwnProperty("changingThisBreaksApplicationSecurity") && /^<p>\d+\./.test(html["changingThisBreaksApplicationSecurity"])) {
             html["changingThisBreaksApplicationSecurity"] = "<p>" + html["changingThisBreaksApplicationSecurity"].substr(html["changingThisBreaksApplicationSecurity"].indexOf('.') + 1);
