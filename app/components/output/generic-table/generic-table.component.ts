c823cacdf55d4a6c5b6717dfa40051e12ee3785e
Armin Hueneburg <hueneburg.armin@gmail.com>
2017-07-07 16:57:19 +0200

    fix hsl2rgb algorithm
    some value for completely wrong in the old algorithm (hsl yellow became rgb gray).
    
    Signed-off-by: Armin Hueneburg <hueneburg.armin@gmail.com>

--- app/components/output/generic-table/generic-table.component.ts
+++ app/components/output/generic-table/generic-table.component.ts
@@ -133,27 +133,43 @@ export class GenericTableComponent implements AfterViewChecked, OnChanges {
     }
 
     private hslToRgb(h, s, l){
+        // fix values: 0<=h<360, 0<=s<=1, 0<=l<=1
+        h = h % 360;
+        s = s > 1 ? s / 100 : s;
+        l = l > 1 ? l / 100 : l;
+
+        // algorithm according to http://www.rapidtables.com/convert/color/hsl-to-rgb.htm
+        const c = (1 - Math.abs(2 * l - 1)) * s;
+        const x = c * (1 - Math.abs((h/60) % 2 - 1));
+        const m = l - c/2;
+
+
         let r, g, b;
-
-        if(s == 0){
-            r = g = b = l; // achromatic
-        }else{
-            let hue2rgb = function hue2rgb(p, q, t){
-                if(t < 0) t += 1;
-                if(t > 1) t -= 1;
-                if(t < 1/6) return p + (q - p) * 6 * t;
-                if(t < 1/2) return q;
-                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-                return p;
-            }
-
-            let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            let p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1/3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1/3);
+        if (0 <= h && h < 60) {
+            r = c;
+            g = x;
+            b = 0;
+        } else if (60 <= h && h < 120) {
+            r = x;
+            g = c;
+            b = 0;
+        } else if (120 <= h && h < 180) {
+            r = 0;
+            g = c;
+            b = x;
+        } else if (180 <= h && h < 240) {
+            r = 0;
+            g = x;
+            b = c;
+        } else if (240 <= h && h < 300) {
+            r = x;
+            g = 0;
+            b = c;
+        } else {
+            r = c;
+            g = 0;
+            b = x;
         }
-
-        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+        return [Math.round((r+m)*255), Math.round((g+m)*255), Math.round((b+m)*255)];
     }
 }
