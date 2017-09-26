3a1244e3b3356512e7c1312550a21ded16e70b4d
Armin Hueneburg <hueneburg.armin@gmail.com>
2017-09-27 01:13:40 +0200

    Fix for repeated issues with PRs in index.md
    Signed-off-by: Armin Hueneburg <hueneburg.armin@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,8 @@
 #! /bin/bash
 
-# global preparements for committing into gh-pages
-prepare () {
-  git remote add SSH git@github.com:ultimate-comparisons/ultimate-comparison-BASE.git
-  touch ~/.ssh/github_deployment
-# decrypt ssh key
-  npm install @alrra/travis-scripts
-  $(npm bin)/set-up.ssh --key "$encrypted_08460152d0a3_key" \
-                        --iv "$encrypted_08460152d0a3_iv" \
-                        --path-encrypted-key "id_rsa.enc"
-}
-
-# commit given directory from given branch to gh-pages
-git_stuff () {
-  TR_BUILD_BRANCH="travis_build_branch_that_should_not_be_created_by_anybody_else"
-  git fetch --all
-  git checkout package.json
-  git checkout package-lock.json
-  git checkout -b ${TR_BUILD_BRANCH}
-  git add "$1"
-  git commit -m "Travis commit for $2"
-  git checkout gh-pages
-  git checkout ${TR_BUILD_BRANCH} "$1"
-  git commit -m "Travis commit"
-  git push -f SSH gh-pages
-  git checkout ${CURRENT_BRANCH}
-  git branch -D ${TR_BUILD_BRANCH}
-}
-
 # build a branch different from master
 build_branch () {
   BRANCH=$1
-  prepare
 
   if [[ ! -d prs ]]; then
     mkdir prs
@@ -41,17 +12,44 @@ build_branch () {
 
 # copy build result to final directory
   cp -r www/* prs/${BRANCH}
+  git add prs
 
 # add prs/${BRANCH} to gh-pages
-  git_stuff "prs/${BRANCH}" "${BRANCH}"
+  git commit -m "Travis commit for ${BRANCH}"
+  git checkout -b ${BRANCH}
+  git checkout -f gh-pages
+  git checkout -f ${BRANCH} prs/${BRANCH}
+  git add prs
+
+# update index.md
+  cp README.md index.md
+  echo "# Docs" >> index.md
+  find docs -type f -exec sh -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [$f]($d/$f)"' sh {} >> index.md \;
+
+# insert linebreak in index.md
+  echo "" >> index.md
+
+# add PRs to index.md
+  git checkout -f gh-pages prs
+  echo "# PRs" >> index.md
+  find prs -mindepth 1 -maxdepth 1 -type d -exec sh -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [$f]($d/$f)"' sh {} >> index.md \;
+
+# add index.md to gh-pages
+  git add index.md
+  git commit -m "Travis commit for prs"
+  git push SSH gh-pages
+  git commit -m "Travis commit for ${BRANCH}"
+  git push SSH gh-pages
 }
 
 # build master
-build_master () {
-  prepare
-  
+build_master () {  
 # add docs to gh-pages
-  git_stuff docs master
+  git checkout -f gh-pages
+  git checkout -f master docs
+  git add docs
+  git commit -m "Travis commit for docs on master"
+  git checkout -f master
 
   if [[ ! -d demo ]]; then
     mkdir demo
@@ -60,15 +58,17 @@ build_master () {
   cp -r www/* demo
 
 # add demo to gh-pages
-  git_stuff demo master
-
-  git checkout gh-pages
+  git add demo
+  git commit -m "Travis commit for master"
+  git checkout -f gh-pages
+  git pull SSH
+  git checkout -f master demo
+  git add demo
+  git commit -m "Travis commit for demo on master"
+  git push SSH gh-pages
   git pull
 
-  ls
-  ls prs
-
-  git checkout master README.md
+  git checkout -f master README.md
 
 # add index.md
   cp README.md index.md
@@ -81,14 +81,18 @@ build_master () {
   echo "" >> index.md
 
 # add PRs to index.md
-  git checkout gh-pages prs
+  git checkout -f gh-pages prs
   echo "# PRs" >> index.md
   find prs -mindepth 1 -maxdepth 1 -type d -exec sh -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [$f]($d/$f)"' sh {} >> index.md \;
 
 # add index.md to gh-pages
-  git_stuff "index.md" master
+  git add index.md
+  git commit -m "Travis commit for prs"
+  git push SSH gh-pages
 }
 
+git remote add SSH git@github.com:hueneburg/ultimate-comparison-BASE.git
+git fetch --all
 # decide which functions should be called
 if [[ ${TRAVIS_PULL_REQUEST} != false ]]; then
   CURRENT_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
