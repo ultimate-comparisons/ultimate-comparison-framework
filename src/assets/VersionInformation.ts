98407d685fb0bcf160c5b2f6e0dc7789202ea9ea
Christoph Kleine <kleinech.github@gmail.com>
2018-02-27 15:41:00 +0100

    Remove VersionInformation.ts
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- src/assets/VersionInformation.ts
+++ src/assets/VersionInformation.ts
@@ -1,5 +1,5 @@
 export class VersionInformation {
-    public date = "2018-02-26";
+    public date = "2018-02-27";
     public commit = "1.0.0";
     public link = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/commit/1.0.0";
 }
