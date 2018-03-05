7671e32fa6bfe96b09ca26d474b76124630aa902
Travis CI User <travis@example.org>
2018-03-05 16:37:54 +0000

    Travis commit for prs on master

--- prs/updatePackages/VersionInformation.ts
+++
@@ -1,5 +0,0 @@
-export class VersionInformation {
-    public date = "2018-02-27";
-    public commit = "1.0.0";
-    public link = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/commit/1.0.0";
-}
