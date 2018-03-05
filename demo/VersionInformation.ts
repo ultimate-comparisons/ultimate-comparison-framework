1100d8a091ae852598e35c656a1f147bbe2ba888
Travis CI User <travis@example.org>
2018-03-05 13:13:10 +0000

    Travis commit for demo on master

--- demo/VersionInformation.ts
+++ demo/VersionInformation.ts
@@ -1,7 +1,5 @@
 export class VersionInformation {
-    public commit = "b5432f52cb518cc8f6d94b3e936a1c6f34e434a6";
-    public commitDate = "2018-02-27";
     public tag = "v2.0.0-alpha.15";
-    public tagDate = "2018-02-27";
-    public link = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.15";
+    public tagDate = "2018-03-05";
+    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.15";
 }
