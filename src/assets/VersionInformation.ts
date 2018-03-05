4b0a54547c97510f2c487c14904a3b316a1bfc77
Christoph Kleine <kleinech.github@gmail.com>
2018-03-05 14:06:32 +0100

    Add npm release script
    - Fix VersionInformation for uc setup/start
    
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- src/assets/VersionInformation.ts
+++ src/assets/VersionInformation.ts
@@ -1,7 +1,5 @@
 export class VersionInformation {
-    public commit = "98407d685fb0bcf160c5b2f6e0dc7789202ea9ea";
-    public commitDate = "2018-02-27";
-    public tag = "1.0.0";
-    public tagDate = "2017-08-31";
-    public link = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/1.0.0";
+    public tag = "v2.0.0-alpha.15";
+    public tagDate = "2018-03-05";
+    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.15";
 }
