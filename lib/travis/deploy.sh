f92504a4f7af6658fda5965e75665115f79b807f
Christoph Kleine <kleinech.github@gmail.com>
2018-03-05 17:34:39 +0100

    Fix deploy script 2
    - Commit changes made to prs folder
    
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- lib/travis/deploy.sh
+++ lib/travis/deploy.sh
@@ -108,6 +108,9 @@ build_master () {
   done
   cd ..
 
+  git add prs
+  git commit -m "Travis commit for prs on master"
+
 # add index.md
   echo "Create index.md"
   echo '<a href="https://github.com/ultimate-comparisons/ultimate-comparison-BASE"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>' > index.md
