9ad36444f33008aa25d4eec58f31f4ac0f0bb23b
Travis CI User <travis@example.org>
2018-03-12 09:54:59 +0000

    Travis commit for prs on master

--- prs/changelogFlavorChange/VersionInformation.ts
+++
@@ -1,5 +0,0 @@
-export class VersionInformation {
-    public tag = "v2.0.0-alpha.16";
-    public tagDate = "2018-03-05";
-    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.16";
-}
