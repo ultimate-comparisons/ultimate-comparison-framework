1c211a1771a31e5c5c66cb7a31dc7701bdb70b5e
Christoph Kleine <kleinech.github@gmail.com>
2018-03-20 13:26:38 +0100

    "Update VersionInformation.ts"

--- src/assets/VersionInformation.ts
+++ src/assets/VersionInformation.ts
@@ -1,5 +1,5 @@
 export class VersionInformation {
-    public tag = "v2.0.0-alpha.23";
+    public tag = "v2.0.0-alpha.24";
     public tagDate = "2018-03-20";
-    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.23";
+    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.24";
 }
