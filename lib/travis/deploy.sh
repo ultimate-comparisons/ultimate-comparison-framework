920c1081f0c743ed80e5c384887d6a4030247b65
Christoph Kleine <kleinech.github@gmail.com>
2018-04-16 16:53:26 +0200

    Remove duplicates and add sh editor config
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- lib/travis/deploy.sh
+++ lib/travis/deploy.sh
@@ -1,5 +1,32 @@
 #! /bin/bash
 
+# build index.md
+buildIndexMd() {
+  echo "Create index.md"
+  echo '<a href="https://github.com/ultimate-comparisons/ultimate-comparison-BASE" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>' > index.md
+    tail -n +2 README.md >> index.md
+
+  # add docs to index.md
+  echo "Create Docs Section"
+  echo "# Docs" >> index.md
+  echo "- [ADR Index](docs/adr)" >> index.md
+
+  # insert linebreak in index.md
+  echo "" >> index.md
+
+  # add PRs to index.md
+  echo "Create PRs Section"
+  git checkout -f gh-pages prs
+  echo "# PRs" >> index.md
+  find prs -mindepth 1 -maxdepth 1 -type d -exec bash -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [${f}](${d}/${f})"' bash {} >> index.md \;
+
+  # add link to current demo to index.md
+  echo "Create link to demo"
+  echo "" >> index.md
+  echo "# Current Master" >> index.md
+  echo "- [Demo](https://ultimate-comparisons.github.io/ultimate-comparison-BASE/demo/)" >> index.md
+}
+
 # build a branch different from master
 build_branch () {
   BRANCH=$1
@@ -10,11 +37,11 @@ build_branch () {
 
   mkdir prs/${BRANCH}
 
-# copy build result to final directory
+  # copy build result to final directory
   cp -r dist/* prs/${BRANCH}
   git add prs
 
-# add prs/${BRANCH} to gh-pages
+  # add prs/${BRANCH} to gh-pages
   git commit -m "Travis commit for ${BRANCH}"
   git checkout -b "${BRANCH}_2"
   git checkout -f gh-pages
@@ -27,42 +54,18 @@ build_branch () {
   git checkout -f "${BRANCH}_2" prs/${BRANCH}
   git add prs
 
-# add index.md
-  echo "Create index.md"
-  echo '<a href="https://github.com/ultimate-comparisons/ultimate-comparison-BASE" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>' > index.md
-  tail -n +2 README.md >> index.md
+  buildIndexMd
 
-# add docs to index.md
-  echo "Create Docs Section"
-  echo "# Docs" >> index.md
-  echo "- [ADR Index](docs/adr)" >> index.md
-
-# insert linebreak in index.md
-  echo "" >> index.md
-
-# add PRs to index.md
-  echo "Create PRs Section"
-  git checkout -f gh-pages prs
-  echo "# PRs" >> index.md
-  find prs -mindepth 1 -maxdepth 1 -type d -exec bash -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [${f}](${d}/${f})"' bash {} >> index.md \;
-
-# add link to current demo to index.md
-  echo "Create link to demo"
-  echo "" >> index.md
-  echo "# Current Master" >> index.md
-  echo "- [Demo](https://ultimate-comparisons.github.io/ultimate-comparison-BASE/demo/)" >> index.md
-
-# add index.md to gh-pages
+  # add index.md to gh-pages
+  echo "Push index.md"
   git add index.md
-  git commit -m "Travis commit for prs"
-  git push SSH gh-pages
   git commit -m "Travis commit for ${BRANCH}"
   git push SSH gh-pages
 }
 
 # build master
 build_master () {
-# add docs to gh-pages
+  # add docs to gh-pages
   git checkout -f gh-pages
   git checkout -f master docs
   git add docs
@@ -76,7 +79,7 @@ build_master () {
 
   cp -r dist/* demo
 
-# add demo to gh-pages
+  # add demo to gh-pages
   git add demo
   git commit -m "Travis commit for master"
   git checkout -f gh-pages
@@ -95,7 +98,7 @@ build_master () {
 
   git checkout -f master README.md
 
-# remove closed prs
+  # remove closed prs
   RESULT="`curl -s https://api.github.com/repos/ultimate-comparisons/ultimate-comparison-BASE/pulls?state=open`"
   LIST="`echo $RESULT | grep -Po '\"ref\": \"([^\"]*)\"' | awk -F':' '{if(match($2, /\"master\"/)){}else{print $2}}'`"
   cd prs
@@ -109,34 +112,9 @@ build_master () {
   git add prs
   git commit -m "Travis commit for prs on master"
 
-# add index.md
-  echo "Create index.md"
-  echo '<a href="https://github.com/ultimate-comparisons/ultimate-comparison-BASE"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>' > index.md
-  tail -n +2 README.md >> index.md
+  buildIndexMd
 
-# add docs to index.md
-  echo "Create Docs Section"
-  echo "# Docs" >> index.md
-  echo "- [Travis_Build_Deploy](docs/Travis_Build_Deploy.md)" >> index.md
-  echo "- [Update_YOUR_Comparison.md](docs/Update_YOUR_Comparison.md)" >> index.md
-  echo "- [ADR Index](docs/adr)" >> index.md
-
-# insert linebreak in index.md
-  echo "" >> index.md
-
-# add PRs to index.md
-  echo "Create PRs Section"
-  git checkout -f gh-pages prs
-  echo "# PRs" >> index.md
-  find prs -mindepth 1 -maxdepth 1 -type d -exec bash -c 'f=$(basename $1 .ts);d=$(dirname $1);echo "- [${f}](${d}/${f})"' bash {} >> index.md \;
-
-# add link to current demo to index.md
-  echo "Create link to demo"
-  echo "" >> index.md
-  echo "# Current Master" >> index.md
-  echo "- [Demo](https://ultimate-comparisons.github.io/ultimate-comparison-BASE/demo/)" >> index.md
-
-# add index.md to gh-pages
+  # add index.md to gh-pages
   echo "Push index.md"
   git add index.md
   git commit -m "Travis commit for prs"
