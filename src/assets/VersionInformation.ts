d18d30dcbdbc98b095a16df577a4ef138f351fd6
Oliver Kopp <kopp.dev@gmail.com>
2020-02-20 14:44:01 +0100

    Prepare release of 2.0.0-alpha.32

--- src/assets/VersionInformation.ts
+++ src/assets/VersionInformation.ts
@@ -1,5 +1,5 @@
 export class VersionInformation {
-    public tag = "v2.0.0-alpha.31";
-    public tagDate = "2018-08-09";
-    public tagLink = "https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.31";
+    public tag = 'v2.0.0-alpha.32';
+    public tagDate = '2020-02-20';
+    public tagLink = 'https://github.com/ultimate-comparisons/ultimate-comparison-BASE/tree/v2.0.0-alpha.32';
 }
